47ef127477e440e08d932d36b706bed5a9731479
Rohan Dugad <rohand2468@gmail.com>
2021-11-13 06:16:06 -0500

    Set to red auton with carousel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAuton.java
@@ -20,35 +20,6 @@ public class RedAuton extends AutonomousControl
     {
         setup(runtime, Goal.setupType.autonomous);
 
-        double speed = 0.5;
-        long timeoutS = 10000;
-        long waitAfter = 0;
-
-        boolean doingCarousel = false;
-        if (doingCarousel) {
-            rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, movements.right);
-            rob.driveTrainEncoderMovement(speed, 12, timeoutS, waitAfter, movements.forward);
-            rob.driveTrainEncoderMovement(speed, 24, timeoutS, waitAfter, movements.cw);
-            rob.runCarouselTimeSpeed(-1, 1500);
-            rob.driveTrainEncoderMovement(speed, 12, timeoutS, waitAfter, movements.left);
-            rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, movements.cw);
-            rob.driveTrainEncoderMovement(speed, 48, timeoutS, waitAfter, movements.left);
-        }
-        else {
-            rob.driveTrainEncoderMovement(speed, 4, timeoutS, waitAfter, movements.backward);
-        }
-
-        rob.driveTrainEncoderMovement(speed, 24, timeoutS, waitAfter, movements.backward);
-        rob.rotate.setPosition(0.5);
-        sleep(500);
-        rob.claw.setPosition(0.2);
-        sleep(500);
-        rob.rotate.setPosition(0);
-        rob.claw.setPosition(0.05);
-        rob.driveTrainEncoderMovement(speed, 20, timeoutS, waitAfter, movements.forward);
-        rob.driveTrainEncoderMovement(speed, 28, timeoutS, waitAfter, movements.cw);
-        rob.driveTrainEncoderMovement(speed/2, 84, timeoutS, waitAfter, movements.forward);
-        rob.driveTrainEncoderMovement(speed, 28, timeoutS, waitAfter, movements.left);
-        rob.driveTrainEncoderMovement(speed, 24, timeoutS, waitAfter, movements.forward);
+        red(true);
     }
 }
