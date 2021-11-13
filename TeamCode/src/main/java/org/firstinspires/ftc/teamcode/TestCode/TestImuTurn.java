da3192a1b9ba5a544560167be11146420fcca2fa
Rohan Dugad <rohand2468@gmail.com>
2021-11-13 00:31:17 -0500

    Made a sketchy 90 degree imu turn

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestImuTurn.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.TestCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Control.AutonomousControl;
+import org.firstinspires.ftc.teamcode.Control.Goal;
+
+@Autonomous(name="TestImuTurn", group="basic")
+public class TestImuTurn extends AutonomousControl
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        setup(runtime, Goal.setupType.autonomous);
+
+        rob.turn(90, Goal.turnside.cw, 0.5, Goal.axis.center);
+    }
+}
