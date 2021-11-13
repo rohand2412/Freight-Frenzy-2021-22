e863dc1c2808c2060b615229e0ec2e5af52ff377
Rohan Dugad <rohand2468@gmail.com>
2021-11-12 20:43:05 -0500

    Implemented test sequence for intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestIntake.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.TestCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Control.AutonomousControl;
+import org.firstinspires.ftc.teamcode.Control.Goal;
+
+@Autonomous(name="TestIntake", group="basic")
+public class TestIntake extends AutonomousControl
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        setup(runtime, Goal.setupType.intake);
+
+        //Suck in field objects
+        rob.runIntakeTimeSpeed(-1, 4000);
+
+        //Raise Intake
+        rob.moveIntakePivotDegrees(1, 30);
+
+        //Push out field objects
+        rob.runIntakeTimeSpeed(1, 4000);
+
+        //Lower Intake
+        rob.moveIntakePivotDegrees(-1, -30);
+    }
+}
