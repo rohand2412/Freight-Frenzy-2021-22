c20fbcb7ade13321a448d4a9767151c9f4d50624
Rohan Dugad <rohand2468@gmail.com>
2021-11-13 06:19:55 -0500

    Added red auton no carousel code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAutonNoCarousel.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Const;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.Control.AutonomousControl;
+import org.firstinspires.ftc.teamcode.Control.Goal;
+import org.firstinspires.ftc.teamcode.Control.Goal.movements;
+
+import static org.firstinspires.ftc.teamcode.Control.Constants.COUNTS_PER_DEGREE_REV_CORE_HEX_MOTOR;
+
+@Autonomous(name="RedAutonNoCarousel", group="basic")
+public class RedAutonNoCarousel extends AutonomousControl
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        setup(runtime, Goal.setupType.autonomous);
+
+        red(false);
+    }
+}
