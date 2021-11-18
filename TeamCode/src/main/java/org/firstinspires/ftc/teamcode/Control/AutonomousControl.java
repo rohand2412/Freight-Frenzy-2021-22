46e83ba16893406dad3bca9fef54816dea72345e
Rohan Dugad <rohand2468@gmail.com>
2021-11-17 19:34:49 -0500

    Tweaked auton code for field at meet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/AutonomousControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/AutonomousControl.java
@@ -6,18 +6,19 @@ public abstract class AutonomousControl extends Central {
     static long waitAfter = 0;
 
     public void blue(boolean doingCarousel) throws InterruptedException {
+        rob.moveLinearSlideInches(1, 1);
         if (doingCarousel) {
             rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, Goal.movements.forward);
             rob.driveTrainEncoderMovement(speed, 12, timeoutS, waitAfter, Goal.movements.right);
             rob.driveTrainEncoderMovement(speed, 24, timeoutS, waitAfter, Goal.movements.ccw);
-            rob.runCarouselTimeSpeed(1, 1500);
+            rob.runCarouselTimeSpeed(0.5, 3500);
             rob.driveTrainEncoderMovement(speed, 12, timeoutS, waitAfter, Goal.movements.backward);
             rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, Goal.movements.ccw);
             rob.driveTrainEncoderMovement(speed, 48, timeoutS, waitAfter, Goal.movements.backward);
             rob.driveTrainEncoderMovement(speed, 28, timeoutS, waitAfter, Goal.movements.ccw);
         }
         else {
-            rob.driveTrainEncoderMovement(speed, 4, timeoutS, waitAfter, Goal.movements.backward);
+            rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, Goal.movements.backward);
         }
 
         rob.driveTrainEncoderMovement(speed, 24, timeoutS, waitAfter, Goal.movements.backward);
@@ -30,16 +31,20 @@ public abstract class AutonomousControl extends Central {
         rob.driveTrainEncoderMovement(speed, 20, timeoutS, waitAfter, Goal.movements.forward);
         rob.driveTrainEncoderMovement(speed, 28, timeoutS, waitAfter, Goal.movements.ccw);
         rob.driveTrainEncoderMovement(speed/2, 84, timeoutS, waitAfter, Goal.movements.forward);
-        rob.driveTrainEncoderMovement(speed, 24, timeoutS, waitAfter, Goal.movements.right);
-        rob.moveIntakePivotDegrees(-1, -90);
+        rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, Goal.movements.cw);
+        rob.driveTrainEncoderMovement(speed, 14, timeoutS, waitAfter, Goal.movements.right);
+        rob.driveTrainEncoderMovement(speed, 12, timeoutS, waitAfter, Goal.movements.forward);
+        rob.moveIntakePivotDegrees(-1, -85);
+        rob.moveLinearSlideInches(-1, -1);
     }
 
     public void red(boolean doingCarousel) throws InterruptedException {
+        rob.moveLinearSlideInches(1, 1);
         if (doingCarousel) {
             rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, Goal.movements.right);
             rob.driveTrainEncoderMovement(speed, 12, timeoutS, waitAfter, Goal.movements.forward);
             rob.driveTrainEncoderMovement(speed, 24, timeoutS, waitAfter, Goal.movements.cw);
-            rob.runCarouselTimeSpeed(-1, 1500);
+            rob.runCarouselTimeSpeed(-0.5, 3500);
             rob.driveTrainEncoderMovement(speed, 12, timeoutS, waitAfter, Goal.movements.left);
             rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, Goal.movements.cw);
             rob.driveTrainEncoderMovement(speed, 48, timeoutS, waitAfter, Goal.movements.left);
@@ -58,9 +63,11 @@ public abstract class AutonomousControl extends Central {
         rob.driveTrainEncoderMovement(speed, 20, timeoutS, waitAfter, Goal.movements.forward);
         rob.driveTrainEncoderMovement(speed, 28, timeoutS, waitAfter, Goal.movements.cw);
         rob.driveTrainEncoderMovement(speed/2, 84, timeoutS, waitAfter, Goal.movements.forward);
+        rob.driveTrainEncoderMovement(speed, 6, timeoutS, waitAfter, Goal.movements.ccw);
         rob.driveTrainEncoderMovement(speed, 28, timeoutS, waitAfter, Goal.movements.left);
-        rob.driveTrainEncoderMovement(speed, 24, timeoutS, waitAfter, Goal.movements.forward);
-        rob.moveIntakePivotDegrees(-1, -90);
+        rob.driveTrainEncoderMovement(speed, 16, timeoutS, waitAfter, Goal.movements.forward);
+        rob.moveIntakePivotDegrees(-1, -85);
+        rob.moveLinearSlideInches(-1, -1);
     }
 }
 
