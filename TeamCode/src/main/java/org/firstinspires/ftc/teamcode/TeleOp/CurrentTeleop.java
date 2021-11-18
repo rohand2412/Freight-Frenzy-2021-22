d81a2b26f57fee08c651d903570d239abee20940
Rohan Dugad <rohand2468@gmail.com>
2021-11-17 19:38:20 -0500

    Finished teleop for meet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CurrentTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CurrentTeleop.java
@@ -2,13 +2,12 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.Control.Goal;
 import org.firstinspires.ftc.teamcode.Control.TeleOpControl;
+
+import static org.firstinspires.ftc.teamcode.Control.Constants.COUNTS_PER_DEGREE_GOBILDA_30_RPM;
+import static org.firstinspires.ftc.teamcode.Control.Constants.COUNTS_PER_INCH_LINEAR_SLIDE_MOTOR;
+import static org.firstinspires.ftc.teamcode.Control.Constants.DEAD_ZONE_SIZE;
 
 @TeleOp(name="CurrentTeleop", group = "basic")
 public class CurrentTeleop extends TeleOpControl
@@ -18,33 +17,221 @@ public class CurrentTeleop extends TeleOpControl
 
         setup(runtime, Goal.setupType.teleop);
 
+        boolean intakeToggle = true; //true is down, false is up
+        boolean alreadyLowered = false;
+        boolean crawlMode = false;
+        boolean linearSlideUp = false;
+        boolean needToRaise = false;
+        boolean drivetrainInUse;
+        double carouselSpeed = 0.5;
+
         waitForStart();
 
         while (opModeIsActive()) {
             standardGamepadData();
 
-            if (g(0)) {
-                rob.driveTrainMovement(fb, Goal.movements.forward);
-            } else if (g(2)) {
-                rob.driveTrainMovement(fb, Goal.movements.backward);
-            } else if (g(3)) {
-                rob.driveTrainMovement(rl, Goal.movements.right);
-            } else if (g(1)) {
-                rob.driveTrainMovement(rl, Goal.movements.left);
-            } else if (g(4)) {
-                rob.driveTrainMovement(diagonalSpeed, Goal.movements.br);
-            }else if (g(5)) {
-                rob.driveTrainMovement(diagonalSpeed, Goal.movements.bl);
-            }else if (g(6)) {
-                rob.driveTrainMovement(diagonalSpeed, Goal.movements.tl);
-            }else if (g(7)) {
-                rob.driveTrainMovement(diagonalSpeed, Goal.movements.tr);
-            } else if (g(8)) {
-                rob.driveTrainMovement(1, Goal.movements.ccw);
-            } else if (g(9)) {
-                rob.driveTrainMovement(1, Goal.movements.cw);
-            } else {
+            drivetrainInUse = false;
+            if (crawlMode) {
+                if (g(0)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.forward);
+                    drivetrainInUse = true;
+                } else if (g(2)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.backward);
+                    drivetrainInUse = true;
+                } else if (g(3)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.right);
+                    drivetrainInUse = true;
+                } else if (g(1)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.left);
+                    drivetrainInUse = true;
+                } else if (g(4)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.br);
+                    drivetrainInUse = true;
+                } else if (g(5)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.bl);
+                    drivetrainInUse = true;
+                } else if (g(6)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.tl);
+                    drivetrainInUse = true;
+                } else if (g(7)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.tr);
+                    drivetrainInUse = true;
+                } else if (g(8)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.ccw);
+                    drivetrainInUse = true;
+                } else if (g(9)) {
+                    rob.driveTrainMovement(0.2, Goal.movements.cw);
+                    drivetrainInUse = true;
+                } else {
+                    rob.stopDrivetrain();
+                }
+            }
+            else {
+                if (g(0)) {
+                    rob.driveTrainMovement(fb, Goal.movements.forward);
+                    drivetrainInUse = true;
+                } else if (g(2)) {
+                    rob.driveTrainMovement(fb, Goal.movements.backward);
+                    drivetrainInUse = true;
+                } else if (g(3)) {
+                    rob.driveTrainMovement(rl, Goal.movements.right);
+                    drivetrainInUse = true;
+                } else if (g(1)) {
+                    rob.driveTrainMovement(rl, Goal.movements.left);
+                    drivetrainInUse = true;
+                } else if (g(4)) {
+                    rob.driveTrainMovement(diagonalSpeed, Goal.movements.br);
+                    drivetrainInUse = true;
+                } else if (g(5)) {
+                    rob.driveTrainMovement(diagonalSpeed, Goal.movements.bl);
+                    drivetrainInUse = true;
+                } else if (g(6)) {
+                    rob.driveTrainMovement(diagonalSpeed, Goal.movements.tl);
+                    drivetrainInUse = true;
+                } else if (g(7)) {
+                    rob.driveTrainMovement(diagonalSpeed, Goal.movements.tr);
+                    drivetrainInUse = true;
+                } else if (g(8)) {
+                    rob.driveTrainMovement(1, Goal.movements.ccw);
+                    drivetrainInUse = true;
+                } else if (g(9)) {
+                    rob.driveTrainMovement(1, Goal.movements.cw);
+                    drivetrainInUse = true;
+                } else {
+                    rob.stopDrivetrain();
+                }
+            }
+
+            if (drivetrainInUse && needToRaise) {
+                if (intakeToggle) {
+                    rob.moveIntakePivotDegrees(1, 70);
+                    alreadyLowered = false;
+                }
+                rob.runIntakeSpeed(0);
+
+                crawlMode = false;
+                needToRaise = false;
+            }
+
+            if (rt > DEAD_ZONE_SIZE) {
+                if (intakeToggle) rob.runIntakeSpeed(-1);
+            }
+            else if (gamepad1.y) {
+                rob.runIntakeSpeed(0);
+            }
+
+            if (lt > DEAD_ZONE_SIZE) {
+                if (intakeToggle) rob.runIntakeSpeed(0.75);
+            }
+            else if (rob.intake.getPower() > 0 && !needToRaise) {
+                rob.runIntakeSpeed(0);
+            }
+
+            if (rb) {
+                if (rob.linearSlide.getCurrentPosition() < 1665) {
+                    rob.moveLinearSlide(1);
+                }
+            }
+            else if (lb) {
+                if (rob.linearSlide.getCurrentPosition() > 10) {
+                    rob.moveLinearSlide(-1);
+                }
+            }
+            else {
+                rob.moveLinearSlide(0);
+            }
+
+            if (gamepad1.a) {
+                if (intakeToggle) {
+                    rob.moveIntakePivotDegrees(1, 90);
+                    rob.runIntakeSpeed(0);
+                }
+                else {
+                    rob.moveIntakePivotDegrees(-1, -90);
+                }
+                intakeToggle = !intakeToggle;
+            }
+
+            if (gamepad1.b) {
+                crawlMode = !crawlMode;
+            }
+
+            if (gamepad1.dpad_right && rob.claw.getPosition() < 0.2) {
+                rob.rotate.setPosition(-0.001 + rob.rotate.getPosition());
+            }
+            else if (gamepad1.dpad_left) {
+                rob.rotate.setPosition(0.001 + rob.rotate.getPosition());
+            }
+
+            if (gamepad1.dpad_up && rob.rotate.getPosition() >= 0.5) {
+                rob.claw.setPosition(0.2);
+            }
+            else if (gamepad1.dpad_down) {
+                rob.claw.setPosition(0);
+            }
+
+            if (rb2) {
+                carouselSpeed = 0.55;
+            }
+            else if (lb2) {
+                carouselSpeed = 0.5;
+            }
+
+            if (rt2 > DEAD_ZONE_SIZE) {
+                rob.runCarouselSpeed(carouselSpeed);
+            }
+            else if (lt2 > DEAD_ZONE_SIZE) {
+                rob.runCarouselSpeed(-carouselSpeed);
+            }
+            else {
+                rob.runCarouselSpeed(0);
+            }
+
+            if (gamepad2.dpad_down) {
+                int target = rob.pivot.getTargetPosition();
+                rob.moveIntakePivotDegrees(-0.1, -2);
+                rob.pivot.setTargetPosition(target);
+            }
+            else if (gamepad2.dpad_up) {
+                int target = rob.pivot.getTargetPosition();
+                rob.moveIntakePivotDegrees(0.1, 2);
+                rob.pivot.setTargetPosition(target);
+            }
+
+            if (gamepad2.dpad_right) {
+                rob.claw.setPosition(0);
+            }
+
+            if (gamepad2.x) {
+                if (linearSlideUp) {
+                    rob.moveSingleMotorUnits(-1, -1600.0, 1, rob.linearSlide);
+                }
+                else {
+                    rob.moveSingleMotorUnits(1, 1600.0, 1, rob.linearSlide);
+                }
+                linearSlideUp = !linearSlideUp;
+            }
+
+            if (gamepad2.a) {
+                if (!intakeToggle && !alreadyLowered) {
+                    rob.moveIntakePivotDegrees(-1, -70);
+                    alreadyLowered = true;
+                }
+                telemetry.update();
+                rob.runIntakeSpeed(0.65);
+                crawlMode = true;
+                needToRaise = true;
+            }
+
+            if (gamepad2.y) {
                 rob.stopDrivetrain();
+                sleep(250);
+                rob.runIntakeSpeed(0);
+                sleep(250);
+                rob.moveLinearSlide(0);
+                sleep(250);
+                rob.runCarouselSpeed(0);
+                sleep(250);
             }
         }
     }
