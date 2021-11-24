aeb92f017023dda5baea3ad604fedb24ec831e5a
Rohan Dugad <rohand2468@gmail.com>
2021-11-23 19:08:45 -0500

    Created TestUltra.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestUltra.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.TestCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Control.AutonomousControl;
+import org.firstinspires.ftc.teamcode.Control.Goal;
+
+@Autonomous(name="TestIntake", group="basic")
+public class TestUltra extends AutonomousControl
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+    }
+}
