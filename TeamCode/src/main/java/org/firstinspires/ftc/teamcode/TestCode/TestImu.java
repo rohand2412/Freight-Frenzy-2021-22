3600d8764bb1d6287d2b80b037742a8ce46593c1
Rohan Dugad <rohand2468@gmail.com>
2021-11-26 20:23:04 -0500

    Implemented imu wrapper functions from Goal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestImu.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.TestCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.Control.AutonomousControl;
+import org.firstinspires.ftc.teamcode.Control.Goal;
+
+@Autonomous(name = "TestImu", group = "basic")
+public class TestImu extends AutonomousControl
+{
+    @Override
+    public void runOpMode() throws  InterruptedException
+    {
+        setup(runtime, Goal.setupType.imu);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addLine(rob.getUpdatedYaw() + "");
+            telemetry.update();
+        }
+    }
+}
