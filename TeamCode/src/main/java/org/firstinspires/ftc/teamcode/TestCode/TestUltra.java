ab27da71e6f300f3685a5b739063ffdee71eeea7
Rohan Dugad <rohand2468@gmail.com>
2021-11-26 20:23:55 -0500

    Created dashboard for front, right, and left ultrasonic sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestUltra.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestUltra.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.TestCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Control.AutonomousControl;
 import org.firstinspires.ftc.teamcode.Control.Goal;
 
@@ -11,6 +12,13 @@ public class TestUltra extends AutonomousControl
     @Override
     public void runOpMode() throws InterruptedException
     {
+        setup(runtime, Goal.setupType.ultra);
 
+        while (opModeIsActive()) {
+            telemetry.addLine("Front: " + rob.frontUltrasonic.getDistance(DistanceUnit.CM) + "");
+            telemetry.addLine("Right: " + rob.rightUltrasonic.getDistance(DistanceUnit.CM) + "");
+            telemetry.addLine("Left: " + rob.leftUltrasonic.getDistance(DistanceUnit.CM) + "");
+            telemetry.update();
+        }
     }
 }
