408e64cddf2438ebb0b68f1a55ffdbb26f8127d8
Rohan Dugad <rohand2468@gmail.com>
2021-12-08 22:26:23 -0500

    Changed setup mode to drivetrain_system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestDriveTrain.java
@@ -12,13 +12,15 @@ import org.firstinspires.ftc.teamcode.Control.Goal.movements;
 
 import static org.firstinspires.ftc.teamcode.Control.Constants.COUNTS_PER_DEGREE_REV_CORE_HEX_MOTOR;
 
+import java.util.Arrays;
+
 @Autonomous(name="TestDriveTrain", group="basic")
 public class TestDriveTrain extends AutonomousControl
 {
     @Override
     public void runOpMode() throws InterruptedException
     {
-        setup(runtime, Goal.setupType.autonomous);
+        setup(runtime, Goal.setupType.drivetrain_system);
 
         //Test each motor individually (FR, FL, BR, BL)
         for (DcMotor motor : rob.drivetrain) {
