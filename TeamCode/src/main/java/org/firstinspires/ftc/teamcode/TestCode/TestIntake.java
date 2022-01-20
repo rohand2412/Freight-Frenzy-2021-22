583f6ba7625126fc1c452a556648e41c0826b0a3
Rohan Dugad <rohand2468@gmail.com>
2022-01-19 21:02:33 -0500

    Commented intakeClaw lines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestIntake.java
@@ -13,7 +13,7 @@ public class TestIntake extends AutonomousControl
     {
         setup(runtime, Goal.setupType.intake);
 
-        rob.intakeClaw.setPosition(0);
+//        rob.intakeClaw.setPosition(0);
 
 //        //Suck in field objects
         rob.runIntakeTimeSpeed(-1, 4000);
@@ -21,7 +21,7 @@ public class TestIntake extends AutonomousControl
 //        //Push out field objects
 //        rob.runIntakeTimeSpeed(1, 4000);
 
-        rob.intakeClaw.setPosition(1);
+//        rob.intakeClaw.setPosition(1);
 //        rob.cappingPivot.setPosition(0.65);
         sleep(2000);
     }
