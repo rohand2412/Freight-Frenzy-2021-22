b435e781f215c1f30032dc3818511cd0752443c1
Rohan Dugad <rohand2468@gmail.com>
2022-01-26 20:54:21 -0500

    [UNTESTED] Created skeleton of Robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/Robot.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Control;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Robot {
+
+    public static ElapsedTime runtime;
+    public static HardwareMap hardwareMap;
+    public static Telemetry telemetry;
+}
