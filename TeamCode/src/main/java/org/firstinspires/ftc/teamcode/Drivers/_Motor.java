1acd81dd92d6c6d6f5636792fb4afbd5f04cdbfe
Rohan Dugad <rohand2468@gmail.com>
2022-01-26 20:52:53 -0500

    [UNTESTED] Completed motor driver

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/_Motor.java
@@ -0,0 +1,219 @@
+package org.firstinspires.ftc.teamcode.Drivers;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.Control.Robot;
+
+public class _Motor {
+
+    private final String _name;
+    private final Type _type;
+    private final Usage _usage;
+    private final double _WHEEL_DIAMETER_INCHES;
+    private final double _COUNTS_PER_INCH;
+    private final double _COUNTS_PER_DEGREE;
+
+    private DcMotor _motor;
+    private double _speed;
+    private boolean _isBusy;
+    private RunLimiter _runLimiter;
+    private double _startTime;
+    private double _elapsedTime;
+
+    _Motor(String name, Type type, DcMotorSimple.Direction direction, DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+        _name = name;
+        _type = type;
+        _usage = Usage.Circular;
+        _WHEEL_DIAMETER_INCHES = 0;
+        _COUNTS_PER_INCH = 0;
+        _COUNTS_PER_DEGREE = _type._COUNTS/360.0;
+        _config(direction, zeroPowerBehavior);
+    }
+
+    _Motor(String name, Type type, DcMotorSimple.Direction direction, DcMotor.ZeroPowerBehavior zeroPowerBehavior, double wheelDiameterInches) {
+        _name = name;
+        _type = type;
+        _usage = Usage.Linear;
+        _WHEEL_DIAMETER_INCHES = wheelDiameterInches;
+        _COUNTS_PER_INCH = _type._COUNTS/(_WHEEL_DIAMETER_INCHES * Math.PI);
+        _COUNTS_PER_DEGREE = 0;
+        _config(direction, zeroPowerBehavior);
+    }
+
+    public void setTypicalSpeed(double speed) {
+        _speed = speed;
+    }
+
+    public void update() {
+        if (_isBusy) {
+            switch (_runLimiter) {
+                case Magnitude:
+                    if (!_motor.isBusy()) {
+                        resetForNextRun();
+                    }
+                    break;
+                case Time:
+                    if (Robot.runtime.milliseconds() >= _startTime + _elapsedTime) {
+                        resetForNextRun();
+                    }
+                    break;
+            }
+        }
+    }
+
+    public void resetForNextRun() {
+        _isBusy = false;
+        stop();
+        _motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void runSpeed(double speed) {
+        if (!_isBusy) {
+            _motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            _setSpeed(speed);
+        }
+    }
+
+    public void runSpeed() {
+        runSpeed(_speed);
+    }
+
+    public void runDistance(double speed, double distance) {
+        if (!_isBusy && speed != 0 && _usage == Usage.Linear) {
+            _isBusy = true;
+            _runLimiter = RunLimiter.Magnitude;
+            stop();
+            int sign = (speed < 0 || distance < 0 ? -1 : 1);
+            _motor.setTargetPosition(_motor.getCurrentPosition() + (int) (sign * Math.abs(distance) * _COUNTS_PER_INCH));
+            _motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            _setSpeed(sign * Math.abs(speed));
+        }
+        else if (_usage == Usage.Circular) {
+            stop();
+            while (true) {
+                Robot.telemetry.addLine("[ERROR] _Motor.runDistance USED WITH USAGE MODE CIRCULAR");
+                Robot.telemetry.update();
+            }
+        }
+    }
+
+    public void runDistance(double distance) {
+        runDistance(_speed, distance);
+    }
+
+    public void runTime(double speed, double milliseconds) {
+        if (!_isBusy && speed != 0) {
+            _isBusy = true;
+            _runLimiter = RunLimiter.Time;
+            stop();
+            _startTime = Robot.runtime.milliseconds();
+            _elapsedTime = milliseconds;
+            _setSpeed(speed);
+        }
+    }
+
+    public void runTime(double distance) {
+        runTime(_speed, distance);
+    }
+
+    public void runDegrees(double speed, double degrees) {
+        if (!_isBusy && speed != 0 && _usage == Usage.Circular) {
+            _isBusy = true;
+            _runLimiter = RunLimiter.Magnitude;
+            stop();
+            int sign = (speed < 0 || degrees < 0 ? -1 : 1);
+            _motor.setTargetPosition(_motor.getCurrentPosition() + (int) (sign * Math.abs(degrees) * _COUNTS_PER_DEGREE));
+            _motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            _setSpeed(sign * Math.abs(speed));
+        }
+        else if (_usage == Usage.Linear) {
+            stop();
+            while (true) {
+                Robot.telemetry.addLine("[ERROR] _Motor.runDegrees USED WITH USAGE MODE LINEAR");
+                Robot.telemetry.update();
+            }
+        }
+    }
+
+    public void runDegrees(double degrees) {
+        runDegrees(_speed, degrees);
+    }
+
+    public void runRotations(double speed, double rotations) {
+        runDegrees(speed, rotations * 360.0);
+    }
+
+    public void runRotations(double rotations) {
+        runRotations(_speed, rotations);
+    }
+
+    public void stop() {
+        _setSpeed(0);
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public double getWheelDiameterInches() {
+        return _WHEEL_DIAMETER_INCHES;
+    }
+
+    public double getCountsPerInch() {
+        return _COUNTS_PER_INCH;
+    }
+
+    public double getCountsPerDegree() {
+        return _COUNTS_PER_DEGREE;
+    }
+
+    public double getSpeed() {
+        return _speed;
+    }
+
+    public boolean isBusy() {
+        return _isBusy;
+    }
+
+    private void _config(DcMotorSimple.Direction direction, DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+        _isBusy = false;
+        _motor = Robot.hardwareMap.dcMotor.get(_name);
+        _motor.setDirection(direction);
+        _motor.setZeroPowerBehavior(zeroPowerBehavior);
+        _motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        stop();
+    }
+
+    private void _setSpeed(double speed) {
+        _motor.setPower(Math.max(Math.min(_speed, 1), -1));
+    }
+
+    private void _setSpeed() {
+        _setSpeed(_speed);
+    }
+
+    public enum Type {
+        GOBILDA_30_RPM(5281.1),
+        GOBILDA_117_RPM(1425.1),
+        GOBILDA_325_RPM(537.6),
+        GOBILDA_435_RPM(383.6),
+        REV_CORE_HEX_MOTOR(288);
+
+        private final double _COUNTS;
+
+        Type(double counts) { _COUNTS = counts; }
+
+        public double getCounts() { return _COUNTS; }
+    }
+
+    private enum Usage {
+        Linear,
+        Circular
+    }
+
+    public enum RunLimiter {
+        Magnitude,
+        Time,
+    }
+}
