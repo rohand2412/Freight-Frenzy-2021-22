88adf5c8b6c4e9b4a78bd7108b2df13589a7e5b2
Rohan Dugad <rohand2468@gmail.com>
2022-01-28 08:52:32 -0500

    [UNTESTED] Added degree functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/_Servo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/_Servo.java
@@ -9,6 +9,11 @@ public class _Servo {
     private final String _NAME;
     private final double _MAX;
     private final double _MIN;
+    private final double _POSITION_1;
+    private final double _POSITION_2;
+    private final double _ANGLE_1;
+    private final double _ANGLE_2;
+    private final double _POSITION_PER_DEGREE;
 
     private final Servo _servo;
     private double _position;
@@ -19,10 +24,20 @@ public class _Servo {
     private double _lastUpdateTime;
     private boolean _isBusy;
 
-    _Servo(String name, Servo.Direction direction, double min, double max, double start) {
+    _Servo(String name, Servo.Direction direction, double min, double max, double start, double position_1, double angle_1, double position_2, double angle_2) {
         _NAME = name;
         _MAX = max;
         _MIN = min;
+        _POSITION_1 = Math.max(position_1, position_2);
+        _POSITION_2 = Math.min(position_1, position_2);
+        _ANGLE_1 = Math.max(angle_1, angle_2);
+        _ANGLE_2 = Math.min(angle_1, angle_2);
+        if ((_ANGLE_1 - _ANGLE_2) != 0) {
+            _POSITION_PER_DEGREE = (_POSITION_1 - _POSITION_2)/(_ANGLE_1 - _ANGLE_2);
+        }
+        else {
+            _POSITION_PER_DEGREE = 0;
+        }
         _servo = Robot.hardwareMap.servo.get(_NAME);
         _servo.setDirection(direction);
         _setPosition(start);
@@ -54,6 +69,12 @@ public class _Servo {
         }
     }
 
+    public void setDegree(double degree) {
+        if (!_isBusy) {
+            _setDegree(degree);
+        }
+    }
+
     public void setSlowPosition(double position, double increment, double intervalMS) {
         if (!_isBusy) {
             _isBusy = true;
@@ -67,6 +88,14 @@ public class _Servo {
 
     public void setSlowPosition(double position, double increment) {
         setSlowPosition(position, increment, 10);
+    }
+
+    public void setSlowDegree(double degree, double increment, double intervalMS) {
+        setSlowPosition(_degreeToPosition(degree), increment/_POSITION_PER_DEGREE, intervalMS);
+    }
+
+    public void setSlowDegree(double degree, double increment) {
+        setSlowPosition(_degreeToPosition(degree), increment/_POSITION_PER_DEGREE);
     }
 
     public String getName() {
@@ -85,5 +114,21 @@ public class _Servo {
     private void _setPosition(double position) {
         _position = Math.max(Math.min(position, _MAX - _MIN), 0);
         _servo.setPosition(_position + _MIN);
+    }
+
+    private void _setDegree(double degree)
+    {
+        _setPosition(_degreeToPosition(degree));
+    }
+
+    private double _degreeToPosition(double degree) {
+        double position;
+        if (degree <= _ANGLE_2) {
+            position = _POSITION_2 - (_ANGLE_2 - degree) * _POSITION_PER_DEGREE;
+        }
+        else {
+            position = _POSITION_2 + (degree - _ANGLE_2) * _POSITION_PER_DEGREE;
+        }
+        return position;
     }
 }
