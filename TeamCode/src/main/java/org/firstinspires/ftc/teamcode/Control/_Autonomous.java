36544e8249374899a9803b9aef6c54e930093cbe
Rohan Dugad <rohand2468@gmail.com>
2022-01-29 23:44:25 -0500

    [UNTESTED] Created opmode parent skeletons

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/_Autonomous.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.Control;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public abstract class _Autonomous extends OpMode {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/_TeleOp.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.Control;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public abstract class _TeleOp extends OpMode {
+
+}
