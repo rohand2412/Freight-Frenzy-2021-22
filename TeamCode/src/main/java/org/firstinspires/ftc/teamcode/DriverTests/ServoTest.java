1e5aabd503136fc2f7994fb3a9419b4c6f70f3ed
Rohan Dugad <rohand2468@gmail.com>
2022-02-03 20:22:53 -0500

    Completed servo test case

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverTests/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverTests/ServoTest.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Control.Robot;
 import org.firstinspires.ftc.teamcode.Control._Autonomous;
-import org.firstinspires.ftc.teamcode.Drivers._Motor;
 import org.firstinspires.ftc.teamcode.Drivers._Servo;
 
 @Autonomous(name="ServoTest", group="DriverTest")
@@ -14,7 +13,8 @@ public class ServoTest extends _Autonomous {
     private _Servo _right;
     private States _state;
     private boolean _justEntered;
-
+    private double _startTime;
+    private double _elapsedTime;
 
     @Override
     public void init() {
@@ -22,49 +22,96 @@ public class ServoTest extends _Autonomous {
         _right = new _Servo("bucketRight",Servo.Direction.REVERSE, 0, 1, 0,0.17, 90, 0.51, 180);
         _right.setDegree(0);
         _justEntered = true;
+        _state = States.SET_POS;
     }
 
     @Override
     public void loop(){
         _right.update();
         telemetry.addLine(String.valueOf(_right.getName()));
+        telemetry.addLine(String.valueOf(_right.getPosition()));
+        telemetry.addLine(String.valueOf(_right.getDegree()));
+        telemetry.addLine(String.valueOf(_right.isBusy()));
+        telemetry.addLine(_state.name());
 
         switch(_state){
             case SET_POS:
-                if(_justEntered){
-                    _justEntered=false;
+                _right.setPosition(1);
+                _state = States.WAIT_1;
+                break;
+            case WAIT_1:
+                if (_justEntered) {
+                    _justEntered = false;
+                    _startTime = Robot.runtime.milliseconds();
+                    _elapsedTime = 2000;
                 }
-                else if (!_right.isBusy()){
-                    _right.setPosition(0.5);
+                else if (Robot.runtime.milliseconds() >= _startTime + _elapsedTime) {
+                    _state = States.SET_DEG;
+                    _justEntered = true;
                 }
+                break;
             case SET_DEG:
-                if(_justEntered){
+                _right.setDegree(90);
+                _state = States.WAIT_2;
+                break;
+            case WAIT_2:
+                if (_justEntered) {
                     _justEntered = false;
+                    _startTime = Robot.runtime.milliseconds();
+                    _elapsedTime = 2000;
                 }
-                else if(!_right.isBusy()){
-                    _right.setDegree(45);
+                else if (Robot.runtime.milliseconds() >= _startTime + _elapsedTime) {
+                    _state = States.SET_SLOW_POS;
+                    _justEntered = true;
                 }
-            case SET_SLDEG:
-                if(_justEntered){
+                break;
+            case SET_SLOW_POS:
+                if (_justEntered) {
                     _justEntered = false;
+                    _right.setSlowPosition(0.85, 0.005);
                 }
-                else if(!_right.isBusy()){
-                    _right.setSlowDegree(45,0.5);
+                else if (!_right.isBusy()) {
+                    _state = States.SET_SLOW_DEG;
+                    _justEntered = true;
                 }
-            case SET_SLPOS:
-                if(_justEntered){
+                break;
+            case SET_SLOW_DEG:
+                if (_justEntered) {
                     _justEntered = false;
+                    _right.setSlowDegree(90, 1.35);
                 }
-                else if(!_right.isBusy()){
-                    _right.setSlowPosition(0.7,0.8);
+                else if (!_right.isBusy()) {
+                    _state = States.SET_SLOW_DEG_INTERRUPTED;
+                    _justEntered = true;
+                }
+                break;
+            case SET_SLOW_DEG_INTERRUPTED:
+                if (_justEntered) {
+                    _justEntered = false;
+                    _right.setSlowDegree(270, 1.35, 5);
+                }
+                else if (_right.getDegree() >= 225) {
+                    _state = States.STOP;
+                    _justEntered = true;
+                }
+                break;
+            case STOP:
+                if (_justEntered) {
+                    _justEntered = false;
+                    _right.resetForNextRun();
                 }
                 break;
         }
     }
+
     private enum States {
         SET_POS,
+        WAIT_1,
         SET_DEG,
-        SET_SLPOS,
-        SET_SLDEG
+        WAIT_2,
+        SET_SLOW_POS,
+        SET_SLOW_DEG,
+        SET_SLOW_DEG_INTERRUPTED,
+        STOP
     }
 }
