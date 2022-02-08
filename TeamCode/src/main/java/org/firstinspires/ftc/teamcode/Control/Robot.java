04c62f5e85c05b751ae02e123b47b03fb08a79b6
Rohan Dugad <rohand2468@gmail.com>
2022-02-08 18:43:32 -0500

    Implemented PID on crane, tuned cranelift coefs, and privated objs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/Robot.java
@@ -11,6 +11,7 @@ import org.firstinspires.ftc.teamcode.Drivers._Drivetrain;
 import org.firstinspires.ftc.teamcode.Drivers._IMU;
 import org.firstinspires.ftc.teamcode.Drivers._Motor;
 import org.firstinspires.ftc.teamcode.Drivers._OpenCV;
+import org.firstinspires.ftc.teamcode.Drivers._PID;
 import org.firstinspires.ftc.teamcode.Drivers._Servo;
 import org.firstinspires.ftc.teamcode.Drivers._ServoGroup;
 import org.firstinspires.ftc.teamcode.Drivers._TFOD;
@@ -22,27 +23,31 @@ public final class Robot {
     public static HardwareMap hardwareMap;
     public static Telemetry telemetry;
 
-    public static _Drivetrain drivetrain;
-    public static _ServoGroup bucket;
-    public static _Motor intake;
-    public static _Motor craneLift;
-    public static _Motor cranePivot;
-    public static _Motor carousel;
-    public static _OpenCV webcam;
-    public static _Vuforia vuforia;
-    public static _TFOD tfod;
-    public static _IMU imu;
-    public static _IMU craneIMU;
+    private static _Drivetrain _drivetrain;
+    private static _ServoGroup _bucket;
+    private static _Motor _intake;
+    private static _Motor _craneLift;
+    private static _PID _craneLiftPID;
+    private static _Motor _cranePivot;
+    private static _PID _cranePivotPID;
+    private static _Motor _carousel;
+    private static _OpenCV _webcam;
+    private static _Vuforia _vuforia;
+    private static _TFOD _tfod;
+    private static _IMU _imu;
+    private static _IMU _craneIMU;
 
     public static final double MM_PER_INCH = 25.4;
-    public static final double TURN_OFFSET_POSITIVE = 18;
-    public static final double TURN_OFFSET_NEGATIVE = 15;
 
-    public static boolean isTurning = false;
-    public static double startAngle;
-    public static double turnDegrees;
+    private static final double _TURN_OFFSET_POSITIVE = 18;
+    private static final double _TURN_OFFSET_NEGATIVE = 15;
+    private static final double _ENDGAME_CAROUSEL_SPEED = 0.5;
 
-    public static double carouselSpeed = 0.5;
+    private static boolean _isTurning = false;
+    private static double _startAngle;
+    private static double _turnDegrees;
+    private static double _craneLiftSetPoint;
+    private static double _cranePivotSetPoint;
 
     private Robot() {};
 
@@ -120,7 +125,7 @@ public final class Robot {
                 DcMotor.ZeroPowerBehavior.BRAKE, wheelDiameter, true);
         _Motor bl = new _Motor("motorBL", _Motor.Type.GOBILDA_435_RPM, DcMotorSimple.Direction.FORWARD,
                 DcMotor.ZeroPowerBehavior.BRAKE, wheelDiameter, true);
-        drivetrain = new _Drivetrain(fr, fl, br, bl, 1.0);
+        _drivetrain = new _Drivetrain(fr, fl, br, bl, 1.0);
     }
 
     private static void setupBucket() {
@@ -128,144 +133,166 @@ public final class Robot {
                 0.17, 90, 0.51, 180);
         _Servo right = new _Servo("bucketRight", Servo.Direction.REVERSE, 0, 1, 0,
                 0.17, 90, 0.51, 180);
-        bucket = new _ServoGroup(left, right);
+        _bucket = new _ServoGroup(left, right);
     }
 
     private static void setupIntake() {
-        intake = new _Motor("intake", _Motor.Type.GOBILDA_435_RPM, DcMotorSimple.Direction.FORWARD,
+        _intake = new _Motor("intake", _Motor.Type.GOBILDA_435_RPM, DcMotorSimple.Direction.FORWARD,
                 DcMotor.ZeroPowerBehavior.BRAKE, false);
     }
 
     private static void setupCraneLift() {
-        craneLift = new _Motor("craneLift", _Motor.Type.GOBILDA_30_RPM, DcMotorSimple.Direction.REVERSE,
+        _craneLift = new _Motor("craneLift", _Motor.Type.GOBILDA_30_RPM, DcMotorSimple.Direction.REVERSE,
                 DcMotor.ZeroPowerBehavior.BRAKE, false);
+        _craneLiftPID = new _PID(() -> _craneIMU.getRoll(), (double data) -> _craneLift.runSpeed(data), () -> _craneLiftSetPoint,
+                0.025, 0.07, 0, _PID.ProportionalMode.MEASUREMENT, _PID.Direction.DIRECT, 100, -1, 1);
     }
 
     private static void setupCranePivot() {
-        cranePivot = new _Motor("cranePivot", _Motor.Type.GOBILDA_117_RPM, DcMotorSimple.Direction.FORWARD,
+        _cranePivot = new _Motor("cranePivot", _Motor.Type.GOBILDA_117_RPM, DcMotorSimple.Direction.FORWARD,
                 DcMotor.ZeroPowerBehavior.BRAKE, false);
+        _cranePivotPID = new _PID(() -> _craneIMU.getYaw(), (double data) -> _cranePivot.runSpeed(data), () -> _cranePivotSetPoint,
+                0.01, 0.01, 0, _PID.ProportionalMode.MEASUREMENT, _PID.Direction.DIRECT, 100, -1, 1);
     }
 
     private static void setupCarousel() {
-        carousel = new _Motor("carousel", _Motor.Type.GOBILDA_312_RPM, DcMotorSimple.Direction.FORWARD,
+        _carousel = new _Motor("carousel", _Motor.Type.GOBILDA_312_RPM, DcMotorSimple.Direction.FORWARD,
                 DcMotor.ZeroPowerBehavior.BRAKE, true);
     }
 
     private static void setupOpenCV() {
-        webcam = new _OpenCV("Webcam 1", 320, 240);
+        _webcam = new _OpenCV("Webcam 1", 320, 240);
     }
 
     private static void setupVuforia() {
-        vuforia = new _Vuforia("Webcam 1");
+        _vuforia = new _Vuforia("Webcam 1");
     }
 
     private static void setupTFOD() {
-        tfod = new _TFOD(vuforia.getVuforia(), 0.45f, true, 320, 1.1, 16.0/9.0,
+        _tfod = new _TFOD(_vuforia.getVuforia(), 0.45f, true, 320, 1.1, 16.0/9.0,
                 "FreightFrenzy_BCDM.tflite", new String[] {"Ball", "Cube", "Duck", "Marker"});
     }
 
     private static void setupIMU() {
-        imu = new _IMU("imu", false, true);
+        _imu = new _IMU("imu", false, true);
     }
 
     private static void setupCraneIMU() {
-        craneIMU = new _IMU("armImu", true, false);
+        _craneIMU = new _IMU("armImu", true, false);
     }
 
     public static void update() {
-        drivetrain.update();
-        bucket.update();
-        intake.update();
-        craneLift.update();
-        cranePivot.update();
-        carousel.update();
-        imu.update();
-        craneIMU.update();
+        _drivetrain.update();
+        _bucket.update();
+        _intake.update();
+        _craneLift.update();
+        _craneLiftPID.update();
+        _cranePivot.update();
+        _cranePivotPID.update();
+        _carousel.update();
+        _imu.update();
+        _craneIMU.update();
 
-        if (isTurning) {
-            if (Math.abs(turnDegrees) > Math.max(TURN_OFFSET_POSITIVE, TURN_OFFSET_NEGATIVE)) {
-                if (turnDegrees > 0 ? imu.getYaw() - startAngle >= turnDegrees - TURN_OFFSET_POSITIVE : imu.getYaw() - startAngle <= turnDegrees + TURN_OFFSET_NEGATIVE) {
-                    isTurning = false;
+        if (_isTurning) {
+            if (Math.abs(_turnDegrees) > Math.max(_TURN_OFFSET_POSITIVE, _TURN_OFFSET_NEGATIVE)) {
+                if (_turnDegrees > 0 ? _imu.getYaw() - _startAngle >= _turnDegrees - _TURN_OFFSET_POSITIVE : _imu.getYaw() - _startAngle <= _turnDegrees + _TURN_OFFSET_NEGATIVE) {
+                    _isTurning = false;
                 }
             }
             else {
-                if (turnDegrees > 0 ? imu.getYaw() - startAngle >= turnDegrees : imu.getYaw() - startAngle <= turnDegrees) {
-                    isTurning = false;
+                if (_turnDegrees > 0 ? _imu.getYaw() - _startAngle >= _turnDegrees : _imu.getYaw() - _startAngle <= _turnDegrees) {
+                    _isTurning = false;
                 }
             }
 
-            if (!isTurning) {
-                drivetrain.stop();
+            if (!_isTurning) {
+                _drivetrain.stop();
             }
         }
     }
 
     public static void turn(double speed, double degrees, TurnAxis turnAxis) {
-        if (!isTurning && degrees != 0) {
-            isTurning = true;
-            startAngle = imu.getYaw();
-            degrees = turnDegrees;
+        if (!_isTurning && degrees != 0) {
+            _isTurning = true;
+            _startAngle = _imu.getYaw();
+            degrees = _turnDegrees;
 
             switch (turnAxis) {
                 case Center:
-                    drivetrain.runSpeed(speed, degrees > 0 ? _Drivetrain.Movements.cw : _Drivetrain.Movements.ccw);
+                    _drivetrain.runSpeed(speed, degrees > 0 ? _Drivetrain.Movements.cw : _Drivetrain.Movements.ccw);
                     break;
                 case Back:
-                    drivetrain.runSpeed(speed, degrees > 0 ? _Drivetrain.Movements.cwback : _Drivetrain.Movements.ccwback);
+                    _drivetrain.runSpeed(speed, degrees > 0 ? _Drivetrain.Movements.cwback : _Drivetrain.Movements.ccwback);
                     break;
                 case Front:
-                    drivetrain.runSpeed(speed, degrees > 0 ? _Drivetrain.Movements.cwfront : _Drivetrain.Movements.ccwfront);
+                    _drivetrain.runSpeed(speed, degrees > 0 ? _Drivetrain.Movements.cwfront : _Drivetrain.Movements.ccwfront);
                     break;
             }
         }
     }
 
+    public static void setCraneLiftDegree(double degree) {
+        _craneLiftSetPoint = degree;
+    }
+
+    public static void setCranePivotDegree(double degree) {
+        _cranePivotSetPoint = degree;
+    }
+
     public static _Drivetrain getDrivetrain() {
-        return drivetrain;
+        return _drivetrain;
     }
 
     public static _ServoGroup getBucket() {
-        return bucket;
+        return _bucket;
     }
 
     public static _Motor getIntake() {
-        return intake;
+        return _intake;
+    }
+
+    public static _PID getCraneLiftPID() {
+        return _craneLiftPID;
     }
 
     public static _Motor getCraneLift() {
-        return craneLift;
+        return _craneLift;
+    }
+
+    public static _PID getCranePivotPID() {
+        return _cranePivotPID;
     }
 
     public static _Motor getCranePivot() {
-        return cranePivot;
+        return _cranePivot;
     }
 
     public static _Motor getCarousel() {
-        return carousel;
+        return _carousel;
     }
 
     public static _OpenCV getWebcam() {
-        return webcam;
+        return _webcam;
     }
 
     public static _Vuforia getVuforia() {
-        return vuforia;
+        return _vuforia;
     }
 
     public static _TFOD getTFOD() {
-        return tfod;
+        return _tfod;
     }
 
     public static _IMU getIMU() {
-        return imu;
+        return _imu;
     }
 
     public static _IMU getCraneIMU() {
-        return craneIMU;
+        return _craneIMU;
     }
 
     public static boolean isTurning() {
-        return isTurning;
+        return _isTurning;
     }
 
     public enum SetupType {
