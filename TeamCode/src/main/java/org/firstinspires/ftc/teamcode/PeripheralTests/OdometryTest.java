b63a5f2b16abad916b4867b6882d767405336a4d
Rohan Dugad <rohand2468@gmail.com>
2022-02-08 18:40:18 -0500

    Reset counts every time program is run

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PeripheralTests/OdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PeripheralTests/OdometryTest.java
@@ -8,23 +8,30 @@ import org.firstinspires.ftc.teamcode.Control._Autonomous;
 @Autonomous(name="OdometryTest", group="PeripheralTest")
 public class OdometryTest extends _Autonomous {
 
+    private int _leftCounts;
+    private int _middleCounts;
+    private int _rightCounts;
+
     @Override
     public void init() {
-        Robot.setup(hardwareMap, telemetry, Robot.SetupType.Carousel, Robot.SetupType.CraneLift, Robot.SetupType.Intake);
+        Robot.setup(hardwareMap, telemetry, Robot.SetupType.CranePivot, Robot.SetupType.CraneLift, Robot.SetupType.Intake);
     }
 
     @Override
     public void init_loop() {}
 
     @Override
-    public void start() {}
+    public void start() {
+        _leftCounts = Robot.getCraneLift().getCounts();
+        _middleCounts = Robot.getCranePivot().getCounts();
+        _rightCounts = Robot.getIntake().getCounts();
+    }
 
     @Override
     public void loop() {
-        telemetry.addLine("Left: " + Robot.getCranePivot().getCounts());
-        telemetry.addLine("Middle: " + Robot.getCraneLift().getCounts());
-        telemetry.addLine("Right: " + Robot.getIntake().getCounts());
-        telemetry.addLine("I can download");
+        telemetry.addLine("Left: " + (Robot.getCraneLift().getCounts() - _leftCounts));
+        telemetry.addLine("Middle: " + (Robot.getCranePivot().getCounts() - _middleCounts));
+        telemetry.addLine("Right: " + (Robot.getIntake().getCounts() - _rightCounts));
     }
 
     @Override
